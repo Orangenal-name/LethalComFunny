0371ef5bd149818c1839a1da2834932546558a43
Orangenal name <alex.w.wells@gmail.com>
2024-01-17 20:04:04 +0000

    DID IT WORK THIS TIME

--- LethalCumFunny/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LethalComFunny
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LethalComFunny";
-        public const string PLUGIN_NAME = "LethalComFunny";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
